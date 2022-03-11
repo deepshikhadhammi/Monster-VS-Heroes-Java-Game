ddf6278b19897f89e66719a4e59987917511ac5d
deepshikha.dhammi <deepshikhadhammi@ucalgary.ca>
2022-03-11 09:48:36 -0700

    Commenting for reader file.

--- src/mvh/util/Reader.java
+++ src/mvh/util/Reader.java
@@ -20,45 +20,42 @@ public final class Reader {
         int row = 0, column = 0;
         String line = "";
         try {
-            FileReader file_reader = new FileReader(fileWorld);
+            FileReader file_reader = new FileReader(fileWorld); //read a file
             BufferedReader buffered_reader = new BufferedReader(file_reader);
 
-            for (int i = 0; i < 2; i++) {
+            for (int i = 0; i < 2; i++) {  //read the row and column value from first two lines
                 line = buffered_reader.readLine();
                 if (i == 0) {
-                    row = Integer.parseInt(line);
+                    row = Integer.parseInt(line);   //store the value of row
                 } else {
-                    column = Integer.parseInt(line);
+                    column = Integer.parseInt(line);//store he value of column
                 }
             }
-            World world1 = new World(row, column);
-            for (int p = 0,q=3; p < (row * column); p++,q++) {
+            World world1 = new World(row, column);   //create a new world
+            for (int p = 0,q=3; p < (row * column); p++,q++) {   //loop for(row*column lines)
                 line = buffered_reader.readLine();
-                String [] array;
-                array = line.split(",");
-                if (array.length > 2) {
-                    if (array[2].equals("MONSTER")) {
-                        int r = Integer.parseInt(array[0]);
-                        int c = Integer.parseInt(array[1]);
-                        int health = Integer.parseInt(array[4]);
-                        char symbol = array[3].charAt(0);
-                        char weapon = array[5].charAt(0);
-                        Monster monster = new Monster(health, symbol, WeaponType.getWeaponType(weapon));
-                        world1.addEntity(r, c, monster);
-
-                    } else if (array[2].equals("HERO")) {
-                        int r = Integer.parseInt(array[0]);
-                        int c = Integer.parseInt(array[1]);
-                        int health = Integer.parseInt(array[4]);
-                        char symbol = array[3].charAt(0);
-                        int weapon_strength = Integer.parseInt(array[5]);
-                        int arm_strength = Integer.parseInt(array[6]);
-                        Hero hero = new Hero(health, symbol, weapon_strength, arm_strength);
-                        world1.addEntity(r, c, hero);
-                    }
+                String[] array;
+                array = line.split(",");  //split the elements at , and store in an array
+                if (array.length > 2 && (array[2].equals("MONSTER"))) { //check if the information is about monster
+                    int r = Integer.parseInt(array[0]);  //store row of monster
+                    int c = Integer.parseInt(array[1]);  //store column of monster
+                    int health = Integer.parseInt(array[4]); //store health of monster
+                    char symbol = array[3].charAt(0);  //store symbol of monster
+                    char weapon = array[5].charAt(0);  //store weapon of  monster
+                    Monster monster = new Monster(health, symbol, WeaponType.getWeaponType(weapon));//create a monster object
+                    world1.addEntity(r, c, monster);  //add monster to world1
+                } else if (array.length > 2 && array[2].equals("HERO")) {//check if information is about hero
+                    int r = Integer.parseInt(array[0]);  //store row
+                    int c = Integer.parseInt(array[1]); //store column
+                    int health = Integer.parseInt(array[4]);  //store health
+                    char symbol = array[3].charAt(0); //store symbol
+                    int weapon_strength = Integer.parseInt(array[5]); //stores weapon strength
+                    int arm_strength = Integer.parseInt(array[6]); //stores armour strength
+                    Hero hero = new Hero(health, symbol, weapon_strength, arm_strength); //creates a hero object
+                    world1.addEntity(r, c, hero); //add hero to world
                 }
             }
-            return world1;
+            return world1; //return world1
 
         } catch (IOException ex) {
             ex.printStackTrace();
