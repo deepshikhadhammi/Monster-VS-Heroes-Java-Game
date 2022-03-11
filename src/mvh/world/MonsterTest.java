889825b36ad1ad734ac9afd4ef726da7d2df31d1
deepshikha.dhammi <deepshikhadhammi@ucalgary.ca>
2022-03-10 20:40:46 -0700

    Test2 for attackWhere function in Monster.java.

--- src/mvh/world/MonsterTest.java
+++ src/mvh/world/MonsterTest.java
@@ -14,22 +14,42 @@ class MonsterTest {
 
     @Test
     void attackWhere_test1() {
-          {
-            World world=new World(3,3);
-            Monster monster=new Monster(3,'M', WeaponType.AXE);
-            Hero hero=new Hero(3,'H',10,5);
-            world.addEntity(0,0,Wall.getWall());
-            world.addEntity(0,1,hero);
-            world.addEntity(0,2,null);
-            world.addEntity(1,0,null);
-            world.addEntity(1,1,monster);
-            world.addEntity(1,2,null);
-            world.addEntity(2,0,null);
-            world.addEntity(2,1,Wall.getWall());
-            world.addEntity(2,2,null);
 
-            assertEquals(Direction.getDirection(-1,0),monster.attackWhere(world));
+            World world = new World(3, 3);
+            Monster monster = new Monster(3, 'M', WeaponType.AXE);
+            Hero hero = new Hero(3, 'H', 10, 5);
+            world.addEntity(0, 0, Wall.getWall());
+            world.addEntity(0, 1, hero);
+            world.addEntity(0, 2, null);
+            world.addEntity(1, 0, null);
+            world.addEntity(1, 1, monster);
+            world.addEntity(1, 2, null);
+            world.addEntity(2, 0, null);
+            world.addEntity(2, 1, Wall.getWall());
+            world.addEntity(2, 2, null);
 
-        }
+            assertEquals(Direction.getDirection(-1, 0), monster.attackWhere(world));
+
+
+    }
+    @Test
+        void attackWhere_test2() {
+            {
+                World world=new World(3,3);
+                Monster monster=new Monster(3,'M', WeaponType.AXE);
+                Hero hero=new Hero(3,'H',10,5);
+                world.addEntity(0,0,Wall.getWall());
+                world.addEntity(0,1,null);
+                world.addEntity(0,2,null);
+                world.addEntity(1,0,null);
+                world.addEntity(1,1,monster);
+                world.addEntity(1,2,null);
+                world.addEntity(2,0,null);
+                world.addEntity(2,1,Wall.getWall());
+                world.addEntity(2,2,hero);
+
+                assertEquals(Direction.getDirection(1,1),monster.attackWhere(world));
+
+            }
     }
 }
